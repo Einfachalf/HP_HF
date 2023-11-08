c4cdc9bf6202633fdad8f1abef7abd8c7db10ae1
einfachalf <einfachalf@gmail.com>
2023-11-08 14:58:52 +0100

    bdfhddf

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'replicate.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'replicate.delivery',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/predictions/:id',
+        destination: '/api/predictions/replicate?id=:id',
+      },
+    ]
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,33 +1,85 @@
-// components/chat/ChatComponent.tsx
+import { useState } from 'react'
 
-import React from 'react';
-import { useChat } from 'ai/react';
-import Container from '../components/Container';
+import Head from 'next/head'
+import Image from 'next/image'
 
-const ChatComponent: React.FC = () => {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+const sleep = ms => new Promise(r => setTimeout(r, ms))
+
+export default function Chat() {
+  const [prediction, setPrediction] = useState(null)
+  const [error, setError] = useState(null)
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const response = await fetch('/api/predictions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        prompt: e.target.prompt.value,
+      }),
+    })
+    let prediction = await response.json()
+    if (response.status !== 201) {
+      setError(prediction.detail)
+      return
+    }
+    setPrediction(prediction)
+
+    while (
+      prediction.status !== 'succeeded' &&
+      prediction.status !== 'failed'
+    ) {
+      await sleep(1000)
+      const response = await fetch('/api/predictions/' + prediction.chat)
+      prediction = await response.json()
+      if (response.status !== 200) {
+        setError(prediction.detail)
+        return
+      }
+      console.log({ prediction })
+      setPrediction(prediction)
+    }
+  }
 
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.length > 0
-        ? messages.map(m => (
-            <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === 'user' ? 'User: ' : 'AI: '}
-              {m.content}
-            </div>
-          ))
-        : null}
+    <div className={styles.container}>
+      <Head>
+        <title>Replicate + Next.js</title>
+      </Head>
 
-      <form onSubmit={handleSubmit}>
+      <p>
+        Dream something with{' '}
+        <a href='https://replicate.com/stability-ai/stable-diffusion'>SDXL</a>:
+      </p>
+
+      <form className={styles.form} onSubmit={handleSubmit}>
         <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
+          type='text'
+          name='prompt'
+          placeholder='Enter a prompt to display an image'
         />
+        <button type='submit'>Go!</button>
       </form>
-    </div>
-  );
-};
 
-export default ChatComponent;
+      {error && <div>{error}</div>}
+
+      {prediction && (
+        <div>
+          {prediction.output && (
+            <div className={styles.imageWrapper}>
+              <Image
+                fill
+                src={prediction.output[prediction.output.length - 1]}
+                alt='output'
+                sizes='100vw'
+              />
+            </div>
+          )}
+          <p>status: {prediction.status}</p>
+        </div>
+      )}
+    </div>
+  )
+}
